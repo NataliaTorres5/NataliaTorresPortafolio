92a9393607bb716676a6262dbbcd47a41e906ced
Natalia Torres <nataliatorres88@gmail.com>
2023-04-21 13:15:00 -0500

    app file update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Index from './pages/Index';
 import NavBar from './components/NavBar/NavBar';
 import Intro from './components/Intro/Intro';
 import AboutMe from './components/AboutMe/AboutMe';
+import Languages from './components/Languages/Languages';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <NavBar/>
-       <Intro/>
-       <AboutMe/>
-  
+        <NavBar />
+        <Intro />
+        <AboutMe />
+        <Languages />
+
       </header>
     </div>
   );
